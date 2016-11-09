b5fddd48a9f5a46fad5a1646aab293ba01fcad30
Chris Dell <cjdell@gmail.com>
2016-11-09 17:37:15 +0000

    first commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,41 @@
+var seleniumServer = require('selenium-server')
+var phantomjs = require('phantomjs-prebuilt')
+var chromedriver = require('chromedriver')
+
+require('nightwatch-cucumber')()
+
+module.exports = {
+  output_folder: 'reports',
+  custom_commands_path: '',
+  custom_assertions_path: '',
+  globals_path: 'nightwatch.globals.js',
+  live_output: false,
+  disable_colors: false,
+
+  selenium: {
+    start_process: false
+  },
+
+  test_settings: {
+    "default": {
+      "selenium_port": 9515,
+      "selenium_host": "localhost",
+      "default_path_prefix": "",
+
+      screenshots: {
+        enabled: true,
+        on_failure: true,
+        on_error: false,
+        path: 'screenshots/default'
+      },
+
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "chromeOptions": {
+          "args": ["--no-sandbox"]
+        },
+        "acceptSslCerts": true
+      }
+    }
+  }
+}
